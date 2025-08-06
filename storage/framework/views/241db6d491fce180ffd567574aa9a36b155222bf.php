fba8b23e34cd222f62287aeed828b081746c6cd1
ReySptn <164753260+ReySptn@users.noreply.github.com>
2025-08-06 15:11:27 +0700

    Update color

--- storage/framework/views/241db6d491fce180ffd567574aa9a36b155222bf.php
+++ storage/framework/views/241db6d491fce180ffd567574aa9a36b155222bf.php
@@ -1,3 +1,5 @@
+
+
 <?php $__env->startSection('container'); ?>
 <style>
     /* --- CSS Styles copied from other dashboards for consistency --- */
@@ -284,7 +286,7 @@
                                 <th style="width: 3%; text-align: center;">
                                     <input class="form-check-input" type="checkbox" id="select-all-checkbox">
                                 </th>
-                                <th style="width: 5%;">#</th>
+                                <th style="width: 5%;">NO</th>
                                 <th style="width: 10%;">Tgl Masuk</th>
                                 <th style="width: 24%;">Nama Barang</th>
                                 <th style="width: 13%;">Jenis</th>
